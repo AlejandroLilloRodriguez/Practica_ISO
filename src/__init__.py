2d1ca67955040a67957aaaa592744c7a0ef9c3aa
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2024-09-25 19:42:54 +0200

    Web scrapping en el siguiente supermercado,Sin terminar todavia

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from plyer import  notification
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -81,6 +80,46 @@ print("\nProductos encontrados:")
 for producto in productos_filtrados:
     print(producto)
 
+def hlml_carrefour(url, palabra_filtro, productos_totales):
+    try:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920x1080")
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        driver.get(url)
+        time.sleep(7)
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            # Desplazarse hacia abajo
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)  # Esperar unos segundos para que carguen los nuevos productos
+
+            # Calcular nueva altura después de desplazarse
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break  # Si no hay más contenido, salir del bucle
+            last_height = new_height
+
+        soup= BeautifulSoup(driver.page_source, 'html.parser') ## guardamos en la variable soup el contenido de la pagina
+        productos2 = soup.find_all('h3', class_='_text_f6lbl_1 _text--m_f6lbl_23') ## buscamos todos los productos de la pagina
+        for bucle in productos2: ## recorremos los datos obtenidos de la lista de productos
+            nombre_producto = bucle.get_text(strip=True) ## Separamos la etiqueta html para poder obtener solo el texto
+            if palabra_filtro.lower() in nombre_producto.lower(): ## comparamos si la palabra que buscamos esta en el nombre del producto
+                productos_totales.append(nombre_producto) ## si esta la añadimos a la lista de productos totales
+        driver.quit() ## cerramos el navegador
+    except:
+        print(f"error")
 
 
+def analisis_url_carrefour (urls, palabra_filtro):
+    productos_totales = [] ## Lista donde vamos a ir guardando todos los productos de la url que busquemos
+    for bucle in urls : ## recorremos las urls
+        hlml_carrefour(url,palabra_filtro, productos_totales) ## llamamos a la funcion que nos va a devolver los productos
+
+
+urls_a_analizar_carrefour = ["https://www.carrefour.es/supermercado/productos-frescos/cat20002/c","https://www.carrefour.es/supermercado/la-despensa/cat20001/c",
+                             "https://www.carrefour.es/supermercado/congelados/cat21449123/c","https://www.carrefour.es/supermercado/congelados/cat21449123/c"]
 
