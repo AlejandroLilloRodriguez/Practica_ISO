c53f795b758c7e1b81ef47eeaf65ece55d62b95a
pablogaraay <garayperezpablo@gmail.com>
2024-10-07 16:06:55 +0200

    Arreglo settings.py

--- micomparadorweb/settings.py
+++ micomparadorweb/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ii#+xed2d+kukpn0(b=$7k8^zcpvo4!(@m@#s6shgh35@cpm-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,13 +74,10 @@ WSGI_APPLICATION = 'micomparadorweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+try:
+    from .settings_local import *
+except ImportError:
+    pass
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
