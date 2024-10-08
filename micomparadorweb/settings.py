c122c9274041ae122356747814cf45262ec706a5
Álvaro <alvaro.iniguezdisla@usp.ceu.es>
2024-10-08 18:02:21 +0200

    Imagen de fondo de la pantalla de inicio añadida

--- micomparadorweb/settings.py
+++ micomparadorweb/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = '0)su12lbvwaobj((9!k5ly56umm#1kxla6zp%bia0861odr2+x'
 
 
 # Quick-start development settings - unsuitable for production
