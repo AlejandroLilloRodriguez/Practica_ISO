feb58705e4fe2c64470514154c6f88feeee95ce8
unknown <j.marin18@usp.ceu.es>
2024-10-09 18:06:04 +0200

    carrito creado y añadido como boton en index

--- carrito/views.py
+++ carrito/views.py
@@ -2,35 +2,9 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .models import Product, Cart, CartItem
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def annadir_a_carrito(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    cart, created = Cart.objects.get_or_create(user=request.user)
-    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
-    
-    if not created:
-        cart_item.quantity += 1
-    cart_item.save()
-    
-    return redirect('cart_detail')
+# carrito/views.py
+from django.shortcuts import render
 
-@login_required
-def detalle_carrito(request):
-    cart = get_object_or_404(Cart, user=request.user)
-    items = CartItem.objects.filter(cart=cart)
-    total_price = sum(item.total_price() for item in items)
-    
-    return render(request, 'cart_detail.html', {'items': items, 'total_price': total_price})
+def vista_carrito(request):
+    return render(request, 'carrito.html')
 
-@login_required
-def borrar_de_carrito(request, cart_item_id):
-    cart_item = get_object_or_404(CartItem, id=cart_item_id)
-    cart_item.delete()
-    return redirect('cart_detail')
-
-@login_required
-def actualizar_carrito(request, cart_item_id, quantity):
-    cart_item = get_object_or_404(CartItem, id=cart_item_id)
-    cart_item.quantity = quantity
-    cart_item.save()
-    return redirect('cart_detail')
--- micomparadorweb/settings.py
+++ micomparadorweb/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'supermercados',
     'productos',
     'buscador',
-    'cart'
+    'carrito',
 ]
 
 MIDDLEWARE = [
