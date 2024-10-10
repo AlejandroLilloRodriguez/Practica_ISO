83b10dc6716b2e5973e3398bb593b0c60025e5b6
Claudia <tu_correo@example.com>
2024-10-10 19:06:25 +0200

    Nuevos cambios pestaña usuario, mejora diseño

--- perfil/forms.py
+++ perfil/forms.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 class CambiarPerfilForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']  # Incluye los campos que quieres cambiar
+        fields = ['username', 'email']
         widgets = {
-            'username': forms.TextInput(attrs={'placeholder': 'Nuevo nombre de usuario'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Nuevo correo electrónico'}),
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email address'}),
         }
 
     def clean_username(self):
@@ -15,9 +15,3 @@ class CambiarPerfilForm(forms.ModelForm):
         if User.objects.filter(username=username).exists():
             raise forms.ValidationError("Este nombre de usuario ya está en uso.")
         return username
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Eliminar los errores de validación predeterminados
-        for field in self.fields.values():
-            field.error_messages = {'required': 'Este campo es obligatorio.'}  # Mensaje de error personalizado
