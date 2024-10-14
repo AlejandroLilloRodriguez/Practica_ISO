88bda6ad9a4b2bba11c8d3436406d2c1a0484f6c
unknown <j.marin18@usp.ceu.es>
2024-10-14 12:52:21 +0200

    Suma productos carrito

--- carrito/views.py
+++ carrito/views.py
@@ -48,7 +48,11 @@ def manejar_carrito(request):
                 carrito[producto_id]['cantidad'] += 1
             else:
                 # Añadir producto con cantidad 1
-                carrito[producto_id] = {'nombre': producto_nombre, 'precio': producto_precio_float, 'cantidad': 1, 'imagen': producto_imagen, 'precio_por_kg': producto_precio_por_kg}
+                carrito[producto_id] = {'nombre': producto_nombre, 
+                                        'precio': producto_precio_float, 
+                                        'cantidad': 1, 
+                                        'imagen': producto_imagen, 
+                                        'precio_por_kg': producto_precio_por_kg}
             
             print(f"Carrito actualizado: {carrito}")
 
@@ -80,6 +84,8 @@ def manejar_carrito(request):
             item_cantidad = int(item['cantidad'])  # Asegúrate de que sea un entero
             item_total = item_precio * item_cantidad  # Calcular el total por item
             total += item_total  # Acumular el total
+        print(item)  # Para verificar el contenido de cada item en el carrito
+
 
     print(f"Precio: {item['precio']}, Tipo: {type(item['precio'])}")
     print(f"Cantidad: {item['cantidad']}, Tipo: {type(item['cantidad'])}")
