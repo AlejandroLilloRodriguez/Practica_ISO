8f5176c5f33e4c4faabe5740f091f066eb6e5cce
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2024-11-07 19:44:07 +0100

    formulario para el pago y registro obligatorio para ello hecho

--- comprar/views.py
+++ comprar/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
-def comprar():
-    print('hola')
-# Create your views here.
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required 
+
+@login_required
+
+def comprar(request):
+    # Tu lógica para la vista de compra
+    if not request.user.is_authenticated:
+        return redirect('mi_compra')  # Redirigir al login si el usuario no está autenticado
+
+    # Lógica para el proceso de compra
+    # ...
+
+    return render(request, 'comprar.html')
