a7298d90af48642c5ad9bbbc7a72f2b2a1f01753
pablogaraay <garayperezpablo@gmail.com>
2024-11-25 12:07:00 +0100

    git pull historico. correccion precio en pasarela pago

--- comprar/views.py
+++ comprar/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required 
+from django.db import connection
 
 @login_required
-
 def comprar(request):
-    # Tu lógica para la vista de compra
-    if not request.user.is_authenticated:
-        return redirect('mi_compra')  # Redirigir al login si el usuario no está autenticado
+    # Verificar que el usuario está autenticado
+    usuario_id = request.user.id
 
-    total = request.session.get('total_precio', 0)
-    return render(request, 'comprar.html', {'total_precio': total})
+    # Obtener los items del carrito
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT producto_id, nombre, precio, precio_por_kg, supermercado, imagen, cantidad
+            FROM carrito WHERE usuario_id = %s
+        """, [usuario_id])
+        columns = [col[0] for col in cursor.description]
+        carrito_items = [
+            dict(zip(columns, row)) for row in cursor.fetchall()
+        ]
+        carrito_items = {item['producto_id']: item for item in carrito_items}
+
+    # Calcular el total_precio
+    total_precio = sum(item['precio'] * item['cantidad'] for item in carrito_items.values())
+
+    return render(request, 'comprar.html', {'total_precio': total_precio})
